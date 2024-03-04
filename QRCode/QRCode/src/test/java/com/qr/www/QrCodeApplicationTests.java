506fa91899c1b58f2d10b9077f7ea423d4363c5d
Lakshman Theng <128983708+LakshmanTheng@users.noreply.github.com>
2024-03-04 19:57:06 +0530

    Main Branch

---
+++ QRCode/QRCode/src/test/java/com/qr/www/QrCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qr.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
